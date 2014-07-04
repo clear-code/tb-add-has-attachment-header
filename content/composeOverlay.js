f5eac9a951406b8e8c443fe623e8837edb5a4025
YUKI Hiroshi <shimoda@clear-code.com>
2014-07-04 14:12:38 +0900

    Remove needless codes

--- content/composeOverlay.js
+++ content/composeOverlay.js
@@ -11,41 +11,8 @@ var AddHasAttachmentHeaderCompose = {
     var attachmentsBacket = GetMsgAttachmentElement();
     var attachmentsCount = attachmentsBacket.itemCount;
     return attachmentsCount > 0 ? 'yes' : 'no';
-  },
-
-  init: function() {
-  },
-
-  handleEvent: function(aEvent) {
-    switch (aEvent.type) {
-      case 'compose-window-init':
-        document.documentElement.addEventListener('compose-window-close', this, false);
-        window.addEventListener('unload', this, false);
-        gMsgCompose.RegisterStateListener(this);
-        return;
-
-      case 'compose-window-close':
-        gMsgCompose.UnregisterStateListener(this);
-        return;
-
-      case 'unload':
-        document.documentElement.removeEventListener('compose-window-init', this, false);
-        document.documentElement.removeEventListener('compose-window-close', this, false);
-        window.removeEventListener('unload', this, false);
-        return;
-    }
-  },
-
-  // nsIMsgComposeStateListener
-  NotifyComposeFieldsReady: function() {},
-  NotifyComposeBodyReady: function() {
-    setTimeout(this.init.bind(this), 100);
-  },
-  ComposeProcessDone: function() {},
-  SaveInFolderDone: function() {}
+  }
 };
-
-document.documentElement.addEventListener('compose-window-init', AddHasAttachmentHeaderCompose, false);
 
 (function() {
   var originalRecipients2CompFields = window.Recipients2CompFields;
